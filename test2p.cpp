dcdc89fc4a109df4ddfceb5979be894666a5d273
Cameron Smith <smithc11@rpi.edu>
2023-04-04 11:07:06 -0400

    use meshfield kokkos controller

--- test2p.cpp
+++ test2p.cpp
@@ -1,15 +1,32 @@
 #include <Omega_h_file.hpp>
 #include <Omega_h_library.hpp>
 #include <Omega_h_mesh.hpp>
+#include <MeshField.hpp>
+#include <KokkosController.hpp>
 
 #include <cstdlib>
 
+using ExecutionSpace = Kokkos::DefaultExecutionSpace;
+using MemorySpace = ExecutionSpace::memory_space;
+
 int main(int argc, char** argv) {
   auto lib = Omega_h::Library(&argc, &argv);
-  OMEGA_H_CHECK(argc == 3 || argc == 4);
+  OMEGA_H_CHECK(argc == 3);
   Omega_h::Mesh mesh(&lib);
   Omega_h::binary::read(argv[1], lib.world(), &mesh);
   auto dim = mesh.dim();
-  if (argc == 4) dim = atoi(argv[2]);
-  Omega_h::vtk::write_parallel(argv[argc - 1], &mesh, dim);
+
+  using Ctrlr = Controller::KokkosController<MemorySpace, ExecutionSpace, double*>;
+  Ctrlr c({mesh.nverts()});
+  MeshField::MeshField<Ctrlr> mf(c);
+
+  auto vtxDbls = mf.makeField<0>();
+
+  auto setVtx = KOKKOS_LAMBDA (const int i) {
+    vtxDbls(i) = 42.1;
+  };
+
+  mf.parallel_for({0,0},{10,10}, setVtx, "set_vertex");
+
+  Omega_h::vtk::write_parallel(argv[2], &mesh, dim);
 }
