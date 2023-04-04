86444508bd7de217cd71745ed55acb2d25b142b0
Cameron Smith <smithc11@rpi.edu>
2023-04-04 16:48:44 -0400

    write rank to field

--- testDist.cpp
+++ testDist.cpp
@@ -18,16 +18,16 @@ int main(int argc, char** argv) {
   Omega_h::Mesh mesh(&lib);
   Omega_h::binary::read(argv[1], lib.world(), &mesh);
 
-  using Ctrlr = Controller::KokkosController<MemorySpace, ExecutionSpace, double*>;
+  using Ctrlr = Controller::KokkosController<MemorySpace, ExecutionSpace, int*>;
   Ctrlr c({mesh.nverts()});
   MeshField::MeshField<Ctrlr> mf(c);
 
-  auto vtxDbls = mf.makeField<0>();
+  auto vtxRankId = mf.makeField<0>();
 
+  const int rank = lib.world()->rank();
   auto setVtx = KOKKOS_LAMBDA (const int i) {
-    vtxDbls(i) = 42.1;
+    vtxRankId(i) = rank;
   };
-
   mf.parallel_for({0},{mesh.nverts()}, setVtx, "set_vertex");
 
   //CK
@@ -35,12 +35,12 @@ int main(int argc, char** argv) {
   //vertex has its value become the value on the non-owning processes
  
   //convert the meshfield to an omegah 'tag' for visualization
-  Omega_h::Write<Omega_h::Real> vtxVals(mesh.nverts());
+  Omega_h::Write<Omega_h::LO> vtxVals(mesh.nverts());
   auto mfToOmegah = KOKKOS_LAMBDA (const int i) {
-    vtxVals[i] = vtxDbls(i);
+    vtxVals[i] = vtxRankId(i);
   };
   mf.parallel_for({0},{mesh.nverts()}, mfToOmegah, "meshField_to_omegah");
-  mesh.add_tag(0, "fromMeshFieldDbl", 1, Omega_h::read(vtxVals));
+  mesh.add_tag(0, "fromMeshFieldInt", 1, Omega_h::read(vtxVals));
 
   //write vtk files
   Omega_h::vtk::write_parallel(argv[2], &mesh, mesh.dim());
