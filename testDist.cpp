1a494fc1f764dbdebdc411155a51b35b509eafc6
Christian Kegel <kegelc@cranium.scorec.rpi.edu>
2023-04-18 14:01:38 -0400

    initial dist test

--- testDist.cpp
+++ testDist.cpp
@@ -17,7 +17,6 @@ int main(int argc, char** argv) {
   OMEGA_H_CHECK(argc == 3);
   Omega_h::Mesh mesh(&lib);
   Omega_h::binary::read(argv[1], lib.world(), &mesh);
-
   using Ctrlr = Controller::KokkosController<MemorySpace, ExecutionSpace, int*>;
   Ctrlr c({mesh.nverts()});
   MeshField::MeshField<Ctrlr> mf(c);
@@ -33,7 +32,30 @@ int main(int argc, char** argv) {
   auto dist = mesh.ask_dist(0);
   //TODO - use the dist to synchronize values across the vertices - the 'owner' of each
   //vertex has its value become the value on the non-owning processes
- 
+  
+  auto remote = mesh.ask_owners(0);
+  auto ranks = remote.ranks;
+  auto idxs = remote.idxs;
+  
+  if(rank == 0){
+    std::cout << "Rank " << rank << " Remote Ranks: size - " << ranks.size() << std::endl;
+    std::cout << "Rank " << rank << "  Remote Idxs: size - " << idxs.size() << std::endl;
+    std::cout << "Rank " << rank << " Remote Ranks: data";
+    auto HostRanks = Omega_h::HostRead<Omega_h::I32>(ranks);
+    auto HostIdxs = Omega_h::HostRead<Omega_h::I32>(idxs);
+
+    for(int i = 0; i < ranks.size(); ++i)
+      std::cout << " - (" << HostRanks[i] << ", " << HostIdxs[i] << ")";
+
+    std::cout << std::endl;
+  }
+
+  int width = 1;
+  auto data = Omega_h::Write<Omega_h::Real>(ranks.size(), rank, "");
+  auto recieved_data = dist.exch(Omega_h::Read<Omega_h::Real>(data), width);
+  
+
+
   //convert the meshfield to an omegah 'tag' for visualization
   Omega_h::Write<Omega_h::LO> vtxVals(mesh.nverts());
   auto mfToOmegah = KOKKOS_LAMBDA (const int i) {
