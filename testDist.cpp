71bbc4dbb257be4203fc1b29e6f908629fad6247
ckegel <57967583+CKegel@users.noreply.github.com>
2024-03-27 10:18:22 -0400

    Use kokkos profiling tools to gather performance data.

--- testDist.cpp
+++ testDist.cpp
@@ -6,6 +6,7 @@
 #include <span>
 #include <Kokkos_Core.hpp>
 #include <cstdlib>
+#include <Kokkos_Profiling_ProfileSection.hpp>
 
 using ExecutionSpace = Kokkos::DefaultExecutionSpace;
 using MemorySpace = ExecutionSpace::memory_space;
@@ -18,36 +19,45 @@ int main(int argc, char** argv) {
   OMEGA_H_CHECK(argc == 3);
   Omega_h::Mesh mesh(&lib);
   Omega_h::binary::read(argv[1], lib.world(), &mesh);
-  using Ctrlr = Controller::KokkosController<MemorySpace, ExecutionSpace, int*>;
-  Ctrlr c({mesh.nverts()});
+  using Ctrlr = Controller::KokkosController<MemorySpace, ExecutionSpace, int*, int**>;
+  Ctrlr c({mesh.nverts(), mesh.nverts(), 2});
   MeshField::MeshField<Ctrlr> mf(c);
 
-  auto vtxRankId = mf.makeField<0>();
+  auto vtxRankId = mf.makeField<1>();
 
   const int rank = lib.world()->rank();
   auto setVtx = KOKKOS_LAMBDA (const int i) {
-    vtxRankId(i) = rank;
+    vtxRankId(i, 0) = rank;
+    vtxRankId(i, 1) = rank;
   };
   mf.parallel_for({0},{mesh.nverts()}, setVtx, "set_vertex");
 
   //Use the dist to synchronize values across the vertices - the 'owner' of each
-  Omega_h::Write<Omega_h::LO> fieldWrite(vtxRankId.serialize());
-  int width = 1;
+  Kokkos::Profiling::pushRegion("meshField-Serialize");
+  auto vtxRankIdView = vtxRankId.serialize();
+  Omega_h::Write<Omega_h::LO> fieldWrite(vtxRankIdView);
+  int width = 2;
+  Kokkos::Profiling::popRegion();
+  Kokkos::Profiling::pushRegion("meshField-sync");
   auto syncFieldRead = mesh.sync_array(0, Omega_h::Read(fieldWrite), width);
+  Kokkos::Profiling::popRegion();
  
   //replace meshField data with synced values
+  Kokkos::Profiling::pushRegion("meshField-Deserialize");
 	vtxRankId.deserialize(syncFieldRead.view());
+  Kokkos::Profiling::popRegion();
   //check the meshfield Data against the Omega_H ownership array
   auto owners = mesh.ask_owners(0).ranks;
   double error = 0;
   Kokkos::parallel_reduce ("Reduction", mesh.nverts(), KOKKOS_LAMBDA (const int i, double& update) {
-	  update += abs(owners[i] - vtxRankId(i));
+	  update += abs(owners[i] - vtxRankId(i, 0));
+	  update += abs(owners[i] - vtxRankId(i, 1));
   }, error);
   OMEGA_H_CHECK(error == 0);
   //convert the meshfield to an omegah 'tag' for visualization
   Omega_h::Write<Omega_h::LO> vtxVals(mesh.nverts());
   auto mfToOmegah = KOKKOS_LAMBDA (const int i) {
-    vtxVals[i] = vtxRankId(i);
+    vtxVals[i] = vtxRankId(i, 0);
   };
   mf.parallel_for({0},{mesh.nverts()}, mfToOmegah, "meshField_to_omegah");
   mesh.add_tag(0, "fromMeshFieldInt", 1, Omega_h::read(vtxVals));
